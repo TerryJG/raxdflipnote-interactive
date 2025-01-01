d0604a99fb2ff53da492cc3b680efff3562482e9
Terrance <gibson.terrance.bs@gmail.com>
2025-01-01 04:42:55 -0500

    chore: modified jotai states

--- src/lib/jotaiState.ts
+++ src/lib/jotaiState.ts
@@ -1,24 +1,47 @@
 import { atom } from "jotai";
 import { atomWithStorage } from 'jotai/utils';
 
+// When the user has clicked the Landing Page button
 export const hasStartedGambling = atom(false);
+
+// When the user has watched the intro video
 export const hasFinishedIntro = atom(false);
 
-export const isIdle = atom(false);
+// When the user is idle 
+// export const isIdle = atom(false);
+
+// The number of times the user has gambled
 export const gambleCount = atomWithStorage('gambleCount', 0);
 
-// Derive hasGambledBefore from gambleCount
+// Derive value from gambleCount. This is to track if the user has gambled before. Mainly used for tutorial sequence.
 export const hasGambledBefore = atom(
   (get) => get(gambleCount) >= 7
 );
 
-export const hasFinishedGambling = atom(false); // lol
+// Retrieve the current time of the video from ReactPlayer
+export const currentVideoTimeAtom = atom<number>(0);
 
+// Caption type definition and state
 export type CaptionState = {
-  text: string;
-  duration?: number;
-  transition?: number;
+  text: string | undefined;
   animate?: boolean;
-} | null;  // no caption
+  onEnter?: boolean;
+  onExit?: boolean;
+  onFirst?: boolean;
+  onLast?: boolean;
+};
 
-export const captionStateAtom = atom<CaptionState>(null);
+export const captionStateAtom = atom<CaptionState>({
+  text: undefined,
+  animate: false,
+  onEnter: false,
+  onExit: false,
+  onFirst: false,
+  onLast: false
+});
+
+
+
+
+// You would miss the entire message of this funny side-project if this state is ever used.
+export const hasFinishedGambling = atom(false); // lol
